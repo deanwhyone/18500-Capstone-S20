ffe25aba4e357574df01c1a355c6dc643ad647b3
Eric Chen <echen2@ece012.ece.local.cmu.edu>
2020-02-13 16:58:07 -0500

    added receiver interface

---
+++ src/networking/Receiver.sv
@@ -0,0 +1,27 @@
+/*
+ * 18500 Capstone S20
+ * Eric Chen, Alton Olsen, Deanyone Su
+ * Overall receiver module to decode and receive data serially across 5 wires
+ */
+ `default_nettype none
+
+module Receiver
+	import NetworkPkg::*,
+		   DisplayPkg::*,
+		   GamePkg::*;
+(
+	input logic 	   serial_in_h,
+	input logic 	   serial_in_0,
+	input logic 	   serial_in_1,
+	input logic 	   serial_in_2,
+	input logic 	   serial_in_3,
+	output logic	   update_opponent_data,
+	output logic [3:0] opponent_garbage,
+	output tile_type_t opponent_hold,
+	output tile_type_t opponent_piece_queue	[NEXT_PIECES],
+	output tile_type_t opponent_playfield	[PLAYFIELD_ROWS][PLAYFIELD_COLS],
+	output logic	   opponent_ready,
+	output logic	   opponent_lost
+);
+
+endmodule // Receiver
--- src/networking/Sender.sv
+++ src/networking/Sender.sv
@@ -1,7 +1,7 @@
 /*
  * 18500 Capstone S20
  * Eric Chen, Alton Olsen, Deanyone Su
- * Overall sender module to transmit encoded data serially across 5 wires
+ * Overall sender module to encode and transmit data serially across 5 wires
  */
  `default_nettype none
 
@@ -10,13 +10,13 @@ module Sender
 		   DisplayPkg::*,
 		   GamePkg::*;
 (
-	input logic		  mp_ready,
-	input logic       data_en,
+	input logic		  player_ready,
+	input logic       update_data,
 	input logic [3:0] garbage,
 	input tile_type_t hold,
 	input tile_type_t piece_queue	[NEXT_PIECES],
 	input tile_type_t playfield 	[PLAYFIELD_ROWS][PLAYFIELD_COLS],
-	input logic		  topout,
+	input logic		  top_out,
 	output logic 	  serial_out_h,
 	output logic 	  serial_out_0,
 	output logic 	  serial_out_1,
