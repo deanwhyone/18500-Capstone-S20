9531a7dba933fc04ebec2e72e8db1341679d0815
Eric Chen <echen2@ece012.ece.local.cmu.edu>
2020-02-13 13:17:47 -0500

    created network package

---
+++ src/networking/Sender.sv
@@ -0,0 +1,15 @@
+/*
+ * 18500 Capstone S20
+ * Eric Chen, Alton Olsen, Deanyone Su
+ *
+ */
+ `default_nettype none
+
+module Sender
+	import NetworkPkg::*,
+		   DisplayPkg::*;
+(
+
+);
+
+endmodule // Sender
