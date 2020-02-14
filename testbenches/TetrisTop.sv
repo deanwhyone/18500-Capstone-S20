6731bd049ea2948ffe13fdf59ae3bb6e74d51802
deanwhyone <deanwhyone@gmail.com>
2020-02-14 16:15:00 -0500

    merge with previous commit

--- testbenches/TetrisTop.sv
+++ testbenches/TetrisTop.sv
@@ -88,10 +88,6 @@ module TetrisTop
     logic           VGA_BLANK;
 
     tile_type_t     tile_type           [PLAYFIELD_ROWS][PLAYFIELD_COLS];
-    logic [23:0]    ppd_output_color;
-    logic           ppd_active;
-    logic [23:0]    npd_output_color;
-    logic           npd_active;
 
     logic [ 4:0]    origin_row;
     logic [ 4:0]    origin_row_update;
@@ -253,75 +249,6 @@ module TetrisTop
             end
             soft_drop   <= key_soft_trigger && !soft_drop && soft_drop_armed;
             hard_drop   <= key_hard_trigger && !hard_drop && hard_drop_armed;
-        end
-    end
-
-    always_comb begin
-        LEDR[17:7]              = 11'd0;
-        {VGA_R, VGA_G, VGA_B}   = BG_COLOR;
-        if (SW[16]) begin
-            unique case (tetris_screen)
-                START_SCREEN: begin
-                    if (VGA_row < 10'd240) begin
-                        {VGA_R, VGA_G, VGA_B} = TETROMINO_I_COLOR;
-                    end
-                end
-                SPRINT_MODE: begin
-                    // border color
-                    if (VGA_row >= BORDER_VSTART && VGA_row < BORDER_VEND &&
-                        VGA_col >= BORDER_HSTART && VGA_col < BORDER_HEND) begin
-                        {VGA_R, VGA_G, VGA_B}   = BORDER_COLOR;
-                    end
-                    // use the PPD to light up tiles in the playfield
-                    if (ppd_active) begin
-                        {VGA_R, VGA_G, VGA_B}   = ppd_output_color;
-                    end
-                    // use the NPD to light up tiles in the next tile area
-                    if (npd_active) begin
-                        {VGA_R, VGA_G, VGA_B}   = npd_output_color;
-                    end
-                end
-                MULTIPLAYER_READY: begin
-                    if (VGA_row < 10'd240) begin
-                        {VGA_R, VGA_G, VGA_B} = TETROMINO_T_COLOR;
-                    end
-                end
-                MULTIPLAYER_MODE: begin
-                    if (VGA_row < 10'd240) begin
-                        {VGA_R, VGA_G, VGA_B} = TETROMINO_T_COLOR;
-                    end
-                end
-                GAME_WON: begin
-                    if (VGA_row < 10'd240) begin
-                        {VGA_R, VGA_G, VGA_B} = TETROMINO_S_COLOR;
-                    end
-                end
-                GAME_LOST: begin
-                    if (VGA_row < 10'd240) begin
-                        {VGA_R, VGA_G, VGA_B} = TETROMINO_Z_COLOR;
-                    end
-                end
-            endcase
-
-        end else begin
-            // default to generating test pattern
-            if (VGA_row < 10'd240) begin
-                if ((VGA_col < 10'd160) ||
-                    (VGA_col >= 10'd320 && VGA_col < 10'd480)) begin
-                    VGA_R = 8'd255;
-                end
-
-                if (VGA_col < 10'd320) begin
-                    VGA_G = 8'd255;
-                end
-
-                if ((VGA_col < 10'd80) ||
-                    (VGA_col >= 10'd160 && VGA_col < 10'd240) ||
-                    (VGA_col >= 10'd320 && VGA_col < 10'd400) ||
-                    (VGA_col >= 10'd480 && VGA_col < 10'd560)) begin
-                    VGA_B = 8'd255;
-                end
-            end
         end
     end
 
@@ -550,15 +477,6 @@ module TetrisTop
         .tile_col               (ftr_cols)
     );
 
-    // PPD module
-    PlayfieldPixelDriver ppd_inst (
-        .VGA_row        (VGA_row),
-        .VGA_col        (VGA_col),
-        .tile_type      (tile_type),
-        .output_color   (ppd_output_color),
-        .active         (ppd_active)
-    );
-
     // Seven Bag module
     TheSevenBag seven_bag_inst (
         .clk             (clk),
@@ -567,14 +485,16 @@ module TetrisTop
         .pieces_queue    (next_pieces_queue),
         .the_seven_bag   (LEDR[6:0])
     );
+    assign LEDR[17:7]              = 11'd0;
 
-    // NPD module
-    NextPixelDriver npd_inst (
-        .VGA_row        (VGA_row),
-        .VGA_col        (VGA_col),
-        .pieces_queue   (next_pieces_queue),
-        .output_color   (npd_output_color),
-        .active         (npd_active)
+    GraphicsTop graphics_inst (
+        .VGA_row            (VGA_row),
+        .VGA_col            (VGA_col),
+        .tile_type          (tile_type),
+        .next_pieces_queue  (next_pieces_queue),
+        .testpattern_active (SW[16]),
+        .tetris_screen      (tetris_screen),
+        .output_color       ({VGA_R, VGA_G, VGA_B})
     );
 
     // VGA module
