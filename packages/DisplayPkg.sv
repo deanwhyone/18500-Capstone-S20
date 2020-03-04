ada88ef244f03fc9fa5647e73e502facbe6e83a0
deanwhyone <deanwhyone@gmail.com>
2020-03-03 19:36:38 -0500

    parameters for tspin graphics driver

--- packages/DisplayPkg.sv
+++ packages/DisplayPkg.sv
@@ -58,6 +58,11 @@ package DisplayPkg;
     parameter TIMER_VSTART          = LC_VSTART - 16;
     parameter TIMER_VEND            = LC_VSTART;
 
+    parameter TSPIN_HSTART          = 0;
+    parameter TSPIN_HEND            = HOLD_HSTART - 5;
+    parameter TSPIN_VSTART          = HOLD_VSTART;
+    parameter TSPIN_VEND            = HOLD_VEND;
+
     parameter BG_COLOR              = 24'h40_4040;
     parameter BORDER_COLOR          = 24'hff_ffff;
     parameter TILE_BLANK_COLOR      = 24'h00_0000;
