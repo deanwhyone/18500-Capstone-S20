c4787c220c3397252dff1aeb1a54d8d1a4a85de4
Deanyone Su <deanwhyone@gmail.com>
2020-03-26 23:23:16 -0400

    reduce aggressiveness of white filtering

--- assets/image_matrix_gen.py
+++ assets/image_matrix_gen.py
@@ -27,9 +27,9 @@ for i in range(row_count):
         # print(red_value)
         # print(green_value)
         # print(blue_value)
-        if (int(img_matrix[i][j][0]) > 235 and\
-            int(img_matrix[i][j][0]) > 235 and\
-            int(img_matrix[i][j][0]) > 235):
+        if (int(img_matrix[i][j][0]) > 220 and\
+            int(img_matrix[i][j][1]) > 220 and\
+            int(img_matrix[i][j][2]) > 220):
             red_value = '01000'
             green_value = '010000'
             blue_value = '01000'
