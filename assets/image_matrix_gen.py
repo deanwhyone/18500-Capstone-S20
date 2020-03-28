354f370f26f1fd3c1bceef43c3e072d9ce2f3988
Deanyone Su <deanwhyone@gmail.com>
2020-03-27 23:14:35 -0400

    dropping H back to default, increasing template window

--- assets/image_matrix_gen.py
+++ assets/image_matrix_gen.py
@@ -9,7 +9,7 @@ from sys import argv
 
 # get filename from command line, get RGB values
 img_matrix = cv2.imread(argv[1])
-img_matrix = cv2.fastNlMeansDenoisingColored(img_matrix, None, 20, 10, 7, 21)
+img_matrix = cv2.fastNlMeansDenoisingColored(img_matrix, None, 10, 10, 15, 21)
 color_matrix = []
 
 # scan through img_matrix and scale RGB values by A/255
