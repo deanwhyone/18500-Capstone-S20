2b68e61994447d92cba25a4d3012470ce50782cf
deanwhyone <deanwhyone@gmail.com>
2020-04-12 14:35:18 -0400

    added seven segment drivers to library

--- src/library.sv
+++ src/library.sv
@@ -180,3 +180,50 @@ module AlphanumeralBitMap (
         endcase
     end
 endmodule // AlphanumeralBitMap
+
+module HEXtoSevenSegment
+    (input  logic [3:0] bch,
+     output logic [6:0] segment);
+
+    always_comb begin
+        case (bch)
+            4'd0:       segment = 7'b100_0000;
+            4'd1:       segment = 7'b111_1001;
+            4'd2:       segment = 7'b010_0100;
+            4'd3:       segment = 7'b011_0000;
+            4'd4:       segment = 7'b001_1001;
+            4'd5:       segment = 7'b001_0010;
+            4'd6:       segment = 7'b000_0010;
+            4'd7:       segment = 7'b111_1000;
+            4'd8:       segment = 7'b000_0000;
+            4'd9:       segment = 7'b001_0000;
+            4'd10:      segment = 7'b001_0000;
+            4'd11:      segment = 7'b000_0011;
+            4'd12:      segment = 7'b100_0110;
+            4'd13:      segment = 7'b010_0001;
+            4'd14:      segment = 7'b000_0110;
+            4'd15:      segment = 7'b000_1110;
+            default:    segment = 7'b111_1111;
+        endcase
+    end
+endmodule // HEXtoSevenSegment
+
+module SevenSegmentDigit
+    (input  logic [3:0] bch,
+     output logic [6:0] segment,
+     input  logic       blank);
+
+    logic [6:0] decoded;
+
+    HEXtoSevenSegment b2ss (
+        .bch    (bch),
+        .segment(decoded)
+    );
+
+    always_comb begin
+        if (blank == 1'b1)
+            segment = 7'b111_1111;
+        else
+            segment = decoded;
+    end
+endmodule // SevenSegmentDigit
