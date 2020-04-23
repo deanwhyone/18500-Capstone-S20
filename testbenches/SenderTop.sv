0daee29b331d396c40b0d0c8e6983a3201f69afc
deanwhyone <deanwhyone@gmail.com>
2020-04-23 15:12:59 -0400

    swapped pins to MOSI

--- testbenches/SenderTop.sv
+++ testbenches/SenderTop.sv
@@ -2,12 +2,12 @@
  * 18500 Capstone S20
  * Eric Chen, Alton Olsen, Deanyone Su
  *
- * Top level sender testbench. Switch 17 resets, switch 16
- * must be kept high for the game to be active. Key 3 will
- * send a packet. GPIO clock frequency is 100kHz. Leftmost
+ * Top level sender testbench. Switch 17 resets, Switch 16 enables loading in
+ * tetrominos, low will load in a pre-set pattern. SW[15:14] indicate tetromino
+ * orientation. SW[13:10] indicates the tetromino being loaded.
+ * Key 3 will send a packet. GPIO clock frequency is 100kHz. Leftmost
  * hex display is the number of garbage lines being sent,
- * middle display is the current sequence number. Switches
- * 7-4 toggle the piece filling the playfield, 3-0 toggle
+ * middle display is the current sequence number. Switches 3-0 toggle
  * the number of garbage lines to send.
  *
  **/
@@ -38,7 +38,8 @@ module SenderTop
 
     logic sender_seqNum;
 
-    tile_type_t playfield_piece;
+    logic [ 4:0]    ftr_tile_rows       [4];
+    logic [ 4:0]    ftr_tile_cols       [4];
 
 	Sender sender_inst (.*);
 
@@ -50,32 +51,32 @@ module SenderTop
 
 	assign clk          = CLOCK_50;
 
+    DelayedAutoShiftFSM DAS_send_inst (
+        .clk            (clk),
+        .rst_l          (rst_l),
+        .action_user    (!KEY[3]),
+        .action_valid   (1'b1),
+        .action_out     (update_data)
+    );
+
     always_comb begin
         rst_l           = !SW[17];
-        game_active     = SW[16];
+        game_active     = 1'b1;
         garbage[3:0]    = SW[3:0];
-        playfield_piece = tile_type_t'(SW[7:4]);
-
-        for(int i = 0; i < PLAYFIELD_ROWS; i++) begin
-            for(int j = 0; j < PLAYFIELD_COLS; j++) begin
-                playfield[i][j] = playfield_piece;
-            end
-        end
     end
 
     assign GPIO[0] = clk_gpio;
-    assign GPIO[6] = serial_out_h;
-    assign GPIO[7] = serial_out_0;
-    assign GPIO[8] = serial_out_1;
-    assign GPIO[9] = serial_out_2;
-    assign GPIO[10] = serial_out_3;
+    assign GPIO[1] = serial_out_h;
+    assign GPIO[2] = serial_out_0;
+    assign GPIO[3] = serial_out_1;
+    assign GPIO[4] = serial_out_2;
+    assign GPIO[5] = serial_out_3;
 
-    assign update_data = !KEY[3];
     assign send_ready_ACK = !KEY[2];
     assign send_game_lost = !KEY[1];
 
     always_comb begin
-    	LEDR[17] = 'b0;
+    	LEDR[17] = update_data;
         LEDR[16] = game_active;
         LEDR[15:2] = 'b0;
         LEDR[1] = send_done_h;
@@ -89,5 +90,45 @@ module SenderTop
     HEXtoSevenSegment sevenSeg3 (
         .bch    ({3'b0, sender_seqNum}),
         .segment(HEX3)
+    );
+
+    // drive playfield with test values to send over network
+    always_comb begin
+        // default to empty playfield
+        for (int i = 0; i < PLAYFIELD_ROWS; i++) begin
+            for (int j = 0; j < PLAYFIELD_COLS; j++) begin
+                playfield[i][j] = BLANK;
+            end
+        end
+        if (SW[16]) begin
+            for (int i = 0; i < 4; i++) begin
+                playfield[ftr_tile_rows[i]][ftr_tile_cols[i]] = tile_type_t'(SW[13:10]);
+            end
+        end else begin
+            for (int i = 0; i < PLAYFIELD_ROWS; i++) begin
+                for (int j = 0; j < PLAYFIELD_COLS; j++) begin
+                    case ((i + j) % 8)
+                        0:  playfield[i][j] = BLANK;
+                        1:  playfield[i][j] = I;
+                        2:  playfield[i][j] = O;
+                        3:  playfield[i][j] = T;
+                        4:  playfield[i][j] = J;
+                        5:  playfield[i][j] = L;
+                        6:  playfield[i][j] = S;
+                        7:  playfield[i][j] = Z;
+                    endcase
+                end
+            end
+        end
+    end
+
+    // FTR module
+    FallingTetrominoRender ftr_inst (
+        .origin_row             (5'd10),
+        .origin_col             (5'd4),
+        .falling_type           (tile_type_t'(SW[13:10])),
+        .falling_orientation    (orientation_t'(SW[15:14])),
+        .tile_row               (ftr_tile_rows),
+        .tile_col               (ftr_tile_cols)
     );
 endmodule // SenderTop
